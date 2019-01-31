0150d391c9929486781e7198fbd1f6c78b2b628d
alasalle <angela.lasalle91@gmail.com>
2019-01-30 20:26:09 -0500

    initial set up of enhancer.test.js

---
+++ enhancer.test.js
@@ -0,0 +1,16 @@
+const enhancer = require("./enhancer");
+const { weapons, armor } = require("./items");
+const { longsword, mace, longbow } = weapons;
+const { mailCuirass, steelCuirass, chestGuard } = armor;
+
+describe("the enhancer module", () => {
+  describe("the enhancer success function", () => {
+
+  });
+  describe("the enhancer fail function", () => {
+
+  });
+  describe("the enhancer repair function", () => {
+
+  });
+})
