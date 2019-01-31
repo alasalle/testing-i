486ae45d408a31cca482db3ba77ca37f947ef145
alasalle <angela.lasalle91@gmail.com>
2019-01-30 23:12:16 -0500

    wrote success function

--- enhancer.js
+++ enhancer.js
@@ -1,5 +1,35 @@
+const levels = {16: "PRI", 17: "DUO", 18: "TRI", 19: "TET", 20: "PEN"}
+
 module.exports = {
-  success: item => {},
+  success: item => {
+    if (item.enhancement === 20) {
+      throw new Error('cannot enhance past lvl 20')
+    } else {
+      if (item.enhancement <= 14 && item.durability < 25) {
+        throw new Error('cannot enhance a lvl 14 or lower item when durability is below 25')
+      } else if (item.enhancement > 14 && item.durability < 10) {
+        throw new Error('cannot enhance a lvl 15 or greater item when durability is below 10')
+      } else {
+        const lvl = item.enhancement + 1
+        const enhancementName
+        if (lvl > 15) {
+          enhancementName = levels.lvl
+          return {
+            ...item,
+            enhancement: lvl,
+            name: `[${enhancementName}] ${item.name}`
+          }
+        } else {
+          return {
+            ...item,
+            enhancement: lvl,
+            name: `[+${lvl}] ${item.name}`
+
+          }
+        }
+      }
+    }
+  },
   fail: item => {},
   repair: item => {},
 };
