64503af3f625efc07b2db6c593d4a3f27e019f34
alasalle <angela.lasalle91@gmail.com>
2019-01-30 20:17:14 -0500

    added test items

---
+++ items.js
@@ -0,0 +1,46 @@
+const weapons = {
+  longsword: {
+    name: "Lifebender",
+    type: "weapon",
+    durability: 100,
+    enhancement: 0
+  },
+  mace: {
+    name: "Chaossong",
+    type: "weapon",
+    durability: 100,
+    enhancement: 0
+  },
+  longbow: {
+    name: "Solarflare",
+    type: "weapon",
+    durability: 100,
+    enhancement: 0
+  }
+};
+
+const armor = {
+  mailCuirass: {
+    name: "Mail Cuirass of Timeless Fires",
+    type: "armor",
+    durability: 100,
+    enhancement: 0
+  },
+  steelCuirass: {
+    name: "Steel Cuirass of Binding Memories",
+    type: "armor",
+    durability: 100,
+    enhancement: 0
+  },
+  chestGuard: {
+    name: "Defender's Obsidian Chestguard",
+    type: "armor",
+    durability: 100,
+    enhancement: 0
+  }
+};
+
+module.exports = {
+  weapons,
+  armor
+};
